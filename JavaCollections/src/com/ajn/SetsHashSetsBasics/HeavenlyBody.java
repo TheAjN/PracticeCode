d59008f71ced24c291017ed690bca35d8a227b92
TheAjN <abhisheknaik6@gmail.com>
2021-09-11 01:15:57 +0530

    equals() and hashCode()

--- JavaCollections/src/com/ajn/SetsHashSetsBasics/HeavenlyBody.java
+++ JavaCollections/src/com/ajn/SetsHashSetsBasics/HeavenlyBody.java
@@ -3,11 +3,13 @@ package com.ajn.SetsHashSetsBasics;
 import java.util.HashSet;
 import java.util.Set;
 
+import sun.tools.tree.ThisExpression;
+
 public final class HeavenlyBody {
 
 	private final String name;
 	private final double orbitalperiod;
-	private final Set<HeavenlyBody> satellites;
+	private final Set<HeavenlyBody> satellites;		
 	
 	public HeavenlyBody(String name, double orbitalperiod) {
 		this.name = name;
@@ -22,8 +24,6 @@ public final class HeavenlyBody {
 	public double getOrbitalperiod() {
 		return orbitalperiod;
 	}
-
-	
 	
 	public Set<HeavenlyBody> getSatellites() {
 		return new HashSet<>(this.satellites);
@@ -32,4 +32,54 @@ public final class HeavenlyBody {
 	public boolean addMoon(HeavenlyBody moon) {
 		return this.satellites.add(moon);
 	}
+	
+	
+	
+	
+
+	@Override
+	public boolean equals(/*HeavenlyBody*/Object  obj) { //the reason the error is coming is due to the method getting
+											//overloaded by the built in equals method
+											//to fix this comment HeavenlyBody and replace it to Object
+		if(this == obj) {  //if obj.equals(obj) where both obj are the same
+			return true;
+		}
+		
+		System.out.println("obj.getClass()" + obj.getClass());
+		System.out.println("this.getClass()" + this.getClass());
+		if((obj==null) || (obj.getClass()!=this.getClass())) {
+			return false;
+		}
+		
+		String objName = ((HeavenlyBody) obj).getName();
+		return this.getName().equals(objName);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- JavaCollections/src/com/ajn/SetsHashSetsBasics/Main.java
+++ JavaCollections/src/com/ajn/SetsHashSetsBasics/Main.java
@@ -28,7 +28,102 @@ public class Main {
 		solarSystem.put(tempMoon.getName(), tempMoon);
 		temp.addMoon(tempMoon);		//Since Moon is a heavenlybody, which is also a satellite of Earth,
 									//HeavenlyBody Moon is added as satellite in Heavenlybody Earth
+		
+		temp = new HeavenlyBody("Mars", 4332);
+		solarSystem.put(temp.getName(), temp);
+		planets.add(temp);
+		
 
+		tempMoon = new HeavenlyBody("Deimos", 1.8);
+		solarSystem.put(tempMoon.getName(), tempMoon);
+		temp.addMoon(tempMoon);	
+
+		tempMoon = new HeavenlyBody("Phobos", 1.8);
+		solarSystem.put(tempMoon.getName(), tempMoon);
+		temp.addMoon(tempMoon);
+		
+		
+		temp = new HeavenlyBody("Jupiter", 4332);
+		solarSystem.put(temp.getName(), temp);
+		planets.add(temp);
+		
+		tempMoon = new HeavenlyBody("Io", 1.8);
+		solarSystem.put(tempMoon.getName(), tempMoon);
+		temp.addMoon(tempMoon);
+		
+		tempMoon = new HeavenlyBody("Europa", 1.8);
+		solarSystem.put(tempMoon.getName(), tempMoon); //the reference of the object Europa is added to the
+		temp.addMoon(tempMoon);							// Solar system map & Satellite Set within Heavenlybody
+													//But only one instance of Europa is present
+		tempMoon = new HeavenlyBody("Ganymede", 1.8);
+		solarSystem.put(tempMoon.getName(), tempMoon);
+		temp.addMoon(tempMoon);
+		
+		tempMoon = new HeavenlyBody("Callisto", 1.8);
+		solarSystem.put(tempMoon.getName(), tempMoon);
+		temp.addMoon(tempMoon);
+		
+		temp = new HeavenlyBody("Saturn", 4332);
+		solarSystem.put(temp.getName(), temp);
+		planets.add(temp);
+		
+		temp = new HeavenlyBody("Uranus", 4332);
+		solarSystem.put(temp.getName(), temp);
+		planets.add(temp);
+		
+		
+		temp = new HeavenlyBody("Neptune", 4332);
+		solarSystem.put(temp.getName(), temp);
+		planets.add(temp);
+		
+		temp = new HeavenlyBody("Pluto", 4332);
+		solarSystem.put(temp.getName(), temp);
+		planets.add(temp);
+		
+		
+		
+		System.out.println("Planets");
+		for(HeavenlyBody planet : planets) {
+			System.out.println("\t" + planet.getName());
+		}
+		System.out.println();
+		
+		HeavenlyBody body = solarSystem.get("Jupiter");
+		System.out.println("Satellites of "+body.getName());
+		for (HeavenlyBody satellites : body.getSatellites()) {
+			System.out.println("\t"+satellites.getName());
+			
+		}
+		
+		Set<HeavenlyBody> moons = new HashSet<>();
+		for (HeavenlyBody planet : planets) {
+			moons.addAll(planet.getSatellites());
+			
+		}
+		
+		System.out.println("All the moons");
+		for (HeavenlyBody moon : moons) {
+			System.out.println("\t"+moon.getName());
+		}
+		
+		
+		HeavenlyBody pluto = new HeavenlyBody("Pluto",4332);
+		planets.add(pluto);
+		
+		for (HeavenlyBody planet : planets) {
+				System.out.println(planet.getName()+" : "+planet.getOrbitalperiod()); 
+						//To check if duplicates of pluto is appearing or not, which it does
+					//This problem is caused due to sets/ maps having own objects as a key/element
+				//in their map/set
+			
+		}
+		
+		Object o = new Object();
+		o.equals(o);
+		
+		"Pluto".equals("");
+		
+		
 	}
 
 }
