a2e4a99800752674281425a8d0e00d6f63d5fdcb
TheAjN <abhisheknaik6@gmail.com>
2021-12-25 22:09:10 +0530

    Code challenge StringCharRepeat

---
+++ CodingExercise/src/com/aj/codingExercise/StringCharRepeat.java
@@ -0,0 +1,24 @@
+package com.aj.codingExercise;
+
+import java.util.HashSet;
+
+public class StringCharRepeat {
+
+    public static void main(String[] args) {
+        String sentence = "Abishek";
+
+        char[] chars = sentence.toCharArray();
+
+        HashSet<Character> set = new HashSet<>();
+
+        for(char ch: chars) {
+            set.add(ch);
+        }
+
+        if(set.size() == sentence.length()){
+            System.out.println("duplicate char does not exists");
+        }else{
+            System.out.println("duplicate char does exist");
+        }
+    }
+}
